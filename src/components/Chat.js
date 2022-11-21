019a4d3ef2ded91ac4c740fcc41e2c1d83da3568
ming0714 <86297534@naver.com>
2022-11-21 16:07:55 +0900

    open ai

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useInsertionEffect, useRef, useState } from "react";
+import { Configuration, OpenAIApi } from 'https://cdn.skypack.dev/openai';
+
 const Chat = () => {
     let navigate = useNavigate();
 
@@ -29,15 +31,49 @@ const Chat = () => {
                     <p class="chat_basic">${message.value}</p>
                 </div>
                 <div class="chat_text_div">
-                    <p class="chat_text why">안읽음</p>
                     <p class="chat_text bottom">${hours >= 12 ? "오후" : "오전"} ${hours}:${minutes}</p>
                 </div>
             </li>`;
+            
             const chat_bg = document.querySelector('#chat-bg');
             chat_bg.insertAdjacentHTML('beforeend', template);
             ref.current.scrollTop = ref.current.scrollHeight;
             document.querySelector('#chat-enter').classList.remove('active');
             message.value = "";
+
+
+            const configuration = new Configuration({
+                // apiKey: process.env.OPENAI_API_KEY,
+                apiKey: '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',
+            });
+            const openai = new OpenAIApi(configuration);
+
+            openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: message.value,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            }).then((result) => {
+                let ai_template = 
+                    `<li class="d-flex-l p-rela other-message">
+                        <div class="friend">
+                            <img src=${process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
+                        </div>
+                        <div class="chat_bub">
+                            <p class="user-">짱구귀여워</p>
+                            <p class="chat_basic">${result.data.choices[0].text}</p>
+                        </div>
+                        <div class="chat_text_div">
+                            <p class="chat_text bottom">${hours >= 12 ? "오후" : "오전"} ${hours}:${minutes}</p>
+                        </div>
+                    </li>`;
+                const chat_bg = document.querySelector('#chat-bg');
+                chat_bg.insertAdjacentHTML('beforeend', ai_template);
+                ref.current.scrollTop = ref.current.scrollHeight;
+            })
         }
     }
 
@@ -55,142 +91,45 @@ const Chat = () => {
                 </div>
             </div>
             <div className="chat-inner">
-                <ul className="chat-bg" id="chat-bg">
-                    <li className="d-flex-l p-rela admin">
-                        <div className="chat_bub">
-                            <p className="i-notice"><span className="text-red">개인정보가 포함된 글과 광고성 게시물</span>은 관리자에 의해 <span className="text-red">임의로 삭제</span>될 수 있습니다.</p>
+                <div className="bg">
+                    <ul className="chat-bg" id="chat-bg">
+                        {/* <li className="d-flex-l p-rela admin">
+                            <div className="chat_bub">
+                                <p className="i-notice"><span className="text-red">개인정보가 포함된 글과 광고성 게시물</span>은 관리자에 의해 <span className="text-red">임의로 삭제</span>될 수 있습니다.</p>
+                            </div>
+                        </li> */}
+                        {/* 유저 말풍선 */}
+                        <li className="d-flex-l p-rela my-message">
+                            <div className="chat_bub">
+                                <p className="chat_basic">메세지를 입력해보세요!</p>
+                            </div>
+                            <div className="chat_text_div">
+                                {/* <p className="chat_text why">안읽음</p> */}
+                                <p className="chat_text bottom">오후 13:30</p>
+                            </div>
+                        </li>
+                        {/* 상대방 말풍선 */}
+                        {/* <li className="d-flex-l p-rela other-message">
+                            <div className="friend">
+                                <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
+                            </div>
+                            <div className="chat_bub">
+                                <p className="user-name">짱구귀여워</p>
+                                <p className="chat_basic">반가워요.</p>
+                            </div>
+                            <div className="chat_text_div">
+                                <p className="chat_text">읽음</p>
+                                <p className="chat_text bottom">오후 5:56</p>
+                            </div>
+                        </li> */}
+                        
+                        {/* 입력중 */}
+                        <div className="d-flex-l p-rela text-ing">
+                            <div className="text-ing2"></div>
                         </div>
-                    </li>
-                    {/* 유저 말풍선 */}
-                    <li className="d-flex-l p-rela my-message">
-                        <div className="chat_bub">
-                            <p className="chat_basic">안녕하세요~</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text why">안읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    {/* 상대방 말풍선 */}
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">반가워요.</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela my-message">
-                        <div className="chat_bub">
-                            <p className="chat_basic">잘있어요</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text why">안읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">다시 만나요!</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela my-message">
-                        <div className="chat_bub">
-                            <p className="chat_basic">힘들었던 하루~~ 많이도 지쳤지만~ 우리들 모두 다 하루 일을 시작해~~</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text why">안읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">안녕하세요</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela my-message">
-                        <div className="chat_bub">
-                            <p className="chat_basic">감사해요</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text why">안읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela my-message">
-                        <div className="chat_bub">
-                            <p className="chat_basic">잘 있어요</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text why">안읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">다시 만나요~~</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">힘들었던 하루~~(힘들었던 하루~)</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    <li className="d-flex-l p-rela other-message">
-                        <div className="friend">
-                            <img src={process.env.PUBLIC_URL + "/img/img1.jpg"} alt="user"/>
-                        </div>
-                        <div className="chat_bub">
-                            <p className="user-name">짱구귀여워</p>
-                            <p className="chat_basic">많이도 지쳤지만~~~ 우리들 모두 다~~~</p>
-                        </div>
-                        <div className="chat_text_div">
-                            <p className="chat_text">읽음</p>
-                            <p className="chat_text bottom">오후 5:56</p>
-                        </div>
-                    </li>
-                    {/* 입력중 */}
-                    <div className="d-flex-l p-rela text-ing">
-                        <div className="text-ing2"></div>
-                    </div>
-                </ul>
+                    </ul>
+
+                </div>
             </div>
             <div className="footer-chat">
                 <div className="d-flex chat-content">
